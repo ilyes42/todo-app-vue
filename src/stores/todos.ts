8c4ef6584f177d1c4a894a8384e5353b2a161f09
ilyes <ilyes.hammemi@keyrus.com>
2022-06-21 11:00:04 +0100

    Add markOneTodoAsDone action.

--- src/stores/todos.ts
+++ src/stores/todos.ts
@@ -2,9 +2,9 @@ import { defineStore } from "pinia";
 import type { Todo } from "@/interfaces/Todo";
 
 const INITIAL_TODOS: Array<Todo> = [
-  { id: 1, text: "A" },
-  { id: 2, text: "B" },
-  { id: 3, text: "C" },
+  { id: 1, text: "A", done: false },
+  { id: 2, text: "B", done: false },
+  { id: 3, text: "C", done: false },
 ];
 
 export const useTodosStore = defineStore("todos", {
@@ -15,11 +15,29 @@ export const useTodosStore = defineStore("todos", {
     };
   },
   actions: {
+    /**
+     * Adds a new todo item to the store.
+     * @param {string} text - Todo's text.
+     */
     addNewTodo(text: string) {
-      this.todos.push({ id: this.nextId++, text });
+      this.todos.push({ id: this.nextId++, done: false, text });
     },
+    /**
+     * Deletes a todo item from the store.
+     * @param {number} id - Todo's id.
+     */
     deleteOneTodoById(id: number) {
       this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    /**
+     * Marks one todo as done, provided its id.
+     * @param id - Todo's id.
+     */
+    markOneTodoAsDone(id: number) {
+      const todo = this.todos.find((todo) => todo.id === id);
+      if (todo) {
+        todo.done = true;
+      }
     },
   },
   getters: {
